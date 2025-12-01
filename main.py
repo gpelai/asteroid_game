09545f7d3c50c8c75a4cb4cbf17478c564990fe9
gpelai <gabrielpelaicontato@gmail.com>
2025-12-01 16:18:40 -0300

    add: player class and rendering it into the main widow game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from logger import log_state
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
@@ -9,15 +10,23 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # player instance
+    player = Player(x= SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        tick = clock.tick(60)
+        dt = tick / 1000
 
 if __name__ == "__main__":
     main()
